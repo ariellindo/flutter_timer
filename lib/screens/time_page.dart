d2be132bd620f2d987ea164bee5343ea52b087c2
Ariel Lindo <ariellindo@gmail.com>
2020-06-22 10:04:11 -0500

    added waves BG

--- lib/screens/time_page.dart
+++ lib/screens/time_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_timer/bloc/timer/timer_bloc.dart';
 import 'package:flutter_timer/widgets/actions.dart' as ax;
+import 'package:flutter_timer/widgets/background.dart';
 
 class TimerPage extends StatelessWidget {
   static const TextStyle timerTextStyle = TextStyle(
@@ -17,39 +18,43 @@ class TimerPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter Timer'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 0.0),
-            child: Center(
-              child: BlocBuilder<TimerBloc, TimerState>(
-                builder: (context, state) {
-                  print(state.duration);
-                  final String minutesStr = ((state.duration / 60) % 60)
-                      .floor()
-                      .toString()
-                      .padLeft(2, '0');
-                  final String secondsStr =
-                      (state.duration % 60).floor().toString().padLeft(2, '0');
-                  return Text(
-                    '$minutesStr:$secondsStr',
-                    style: TimerPage.timerTextStyle,
-                  );
-                },
+      body: Stack(
+        children: [
+          Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 0.0),
+                child: Center(
+                  child: BlocBuilder<TimerBloc, TimerState>(
+                    builder: (context, state) {
+                      print(state.duration);
+                      final String minutesStr = ((state.duration / 60) % 60)
+                          .floor()
+                          .toString()
+                          .padLeft(2, '0');
+                      final String secondsStr = (state.duration % 60)
+                          .floor()
+                          .toString()
+                          .padLeft(2, '0');
+                      return Text(
+                        '$minutesStr:$secondsStr',
+                        style: TimerPage.timerTextStyle,
+                      );
+                    },
+                  ),
+                ),
               ),
-            ),
-          ),
-          SizedBox(height: 50.0),
-          BlocBuilder<TimerBloc, TimerState>(
-            condition: (previousState, state) =>
-                state.runtimeType != previousState.runtimeType,
-            builder: (context, state) {
-              return  ax.Actions();
-            }
-
-
+              SizedBox(height: 50.0),
+              BlocBuilder<TimerBloc, TimerState>(
+                  condition: (previousState, state) =>
+                      state.runtimeType != previousState.runtimeType,
+                  builder: (context, state) {
+                    return ax.Actions();
+                  }),
+            ],
           ),
         ],
       ),
